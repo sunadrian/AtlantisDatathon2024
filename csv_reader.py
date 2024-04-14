6b2764e0048191a7e51c56d0488258ba437f3916
Karm Patel <patel.karm0315@gmail.com>
2024-04-13 18:10:05 -0700

    Yay out of debugging hell

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+from collections import namedtuple
 
 def load_dataset(path) -> pd.DataFrame:
     return pd.read_csv(path)
@@ -22,9 +23,46 @@ def calc_zipcode_stats(df: pd.DataFrame) -> dict:
         zipcode_stats[zipcode] = [max_value, min_value, avg_value, avg_latitude, avg_longitude]
     return zipcode_stats
 
+def abs_difference(a, b):
+    return abs(a - b)
 
+def difference(a, b):
+    return a - b
 
+Property_Info = namedtuple('Property_Info', ['address', 'rooms', 'fireplace', 'security', 'sprinklers'])
+def fiveclosest(dataframe, budget, zipcode, budgetcheckbox = False):
+    # storage = {}
+    differences = []
+    associated_info = []
+    cur_df = dataframe[dataframe['ZipCode'] == zipcode]
 
-# if __name__ == "__main__":
-#     df = filereader(Path("PropertyAssessmentData.csv"))
-#     zipcode_stats = calc_zipcode_stats(df)
+    if budgetcheckbox:
+        diff_func = abs_difference
+    else:
+        cur_df = cur_df[cur_df['TotalAssessedValue'] <= budget]
+        diff_func = difference
+        print(cur_df)
+
+    for i, row in cur_df.iterrows():
+        cost = row.iloc[15]
+        new_info = Property_Info(row['PropertyAddress'], row['TotalNumberOfRooms'], row['HasFireplace'],
+                                            row['HasSecurityAlarm'], row['HasSprinklers'])
+        if len(differences) < 5:
+            differences.append(diff_func(budget, cost))
+            associated_info.append(new_info)
+        else:
+            if diff_func(budget, cost) < diff_func(budget, max(differences)):
+                removed_index = differences.index(max(differences))
+                differences.pop(removed_index)
+                differences.append(diff_func(budget, cost))
+                associated_info.pop(removed_index)
+                associated_info.append(new_info)
+    return associated_info
+
+if __name__ == "__main__":
+    df = load_dataset(Path("PropertyAssessmentData.csv"))
+    zipcode_stats = calc_zipcode_stats(df)
+
+    res = fiveclosest(df, 280000, 92706, True)
+    for r in res:
+        print(r)
